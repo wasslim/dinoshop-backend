5288c06f0ccdfda2c6eb52388dad2a9a7bcaad4c
Wassim Fouad <wassim.fouad@student.kdg.be>
2024-07-21 19:21:45 +0200

    nieuwe route voor foto's homepagina

--- server.js
+++ server.js
@@ -12,11 +12,13 @@ const productRoutes = require("./routes/productRoutes");
 const userRoutes = require("./routes/userRoutes");
 const aboutRoutes = require("./routes/aboutRoutes");
 const logoRoutes = require("./routes/logoRoutes");
+const homeRoutes = require("./routes/homeRoutes")
 
 app.use("/products", productRoutes);
 app.use("/users", userRoutes);
 app.use("/about", aboutRoutes);
 app.use("/logo", logoRoutes);
+app.use("/home",homeRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
