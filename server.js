352e608fbf7c2a0e5f89c8c0864d52ea958fe016
Wassim Fouad <wassim.fouad@student.kdg.be>
2024-08-11 17:01:10 +0200

    Update CORS allowed origins

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
-app.use(cors({
-  origin: 'http://localhost:3001', // Replace with your frontend's URL
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
-  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
-}));
 app.use(cors());
 app.use(bodyParser.json());
 
@@ -18,14 +13,12 @@ const userRoutes = require("./routes/userRoutes");
 const aboutRoutes = require("./routes/aboutRoutes");
 const logoRoutes = require("./routes/logoRoutes");
 const homeRoutes = require("./routes/homeRoutes")
-const orderRoutes = require("./routes/orderRoutes")
 
 app.use("/products", productRoutes);
 app.use("/users", userRoutes);
 app.use("/about", aboutRoutes);
 app.use("/logo", logoRoutes);
-app.use("/home", homeRoutes);
-app.use("/order", orderRoutes);
+app.use("/home",homeRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
